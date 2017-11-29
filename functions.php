54ed55a1e3b0fe1ced268c690abb0fefd0b429b5
Miguel <mvalfer92@gmail.com>
2017-11-29 15:45:19 +0000

    asd

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+     * 
+     * Deregister WordPress default jQuery
+     * Register and Enqueue Google CDN jQuery
+     * 
+     */
+    function load_external_jQuery() { // load external file  
+        wp_deregister_script( 'jquery' ); // deregisters the default WordPress jQuery  
+        wp_register_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", array(), false, false);
+        wp_enqueue_script('jquery');
+    }  
+    add_action('wp_enqueue_scripts', 'load_external_jQuery');
+
+function theme_scripts() {
+    
+    // wp_register_script('jqueryboot', get_template_directory_uri() . '/vendor/jquery/jquery.js', array('jquery'));
+    // wp_enqueue_script('jqueryboot');
+     
+    // wp_register_script('bootstrap', get_template_directory_uri() . '/vendor/bootstrap/js/bootstrap.js', array('bootstrapbundle'));
+    // wp_enqueue_script('bootstrap');
+        
+    // wp_register_script('bootstrapmin', get_template_directory_uri() . '/vendor/bootstrap/js/bootstrap.min.js', array('bootstrapbundlemin'));
+    // wp_enqueue_script('bootstrapmin');
+     
+    // wp_register_script('', get_template_directory_uri() . '',array('jquery'));
+    // wp_enqueue_script('');
+    
+    // wp_register_script('bootstrapbundle', get_template_directory_uri() . '/vendor/bootstrap/js/bootstrap.bundle.js', array('jquerybootmin'));
+    // wp_enqueue_script('bootstrapbundle');
+    
+    // wp_register_script('jquerybootmin', get_template_directory_uri() . '/vendor/jquery/jquery.min.js', array('jquery'));
+    // wp_enqueue_script('jquerybootmin');
+    
+    wp_register_script('bootstrapbundlemin', get_template_directory_uri() . '/vendor/bootstrap/js/bootstrap.bundle.min.js', array('jquery'));
+    wp_enqueue_script('bootstrapbundlemin');
+
+}
+
+add_action('wp_enqueue_scripts','theme_scripts');
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Business Casual - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <!--<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!-- Custom fonts for this template -->
+    <!--<link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">-->
+    <!--<link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">-->
+
+    <!-- Custom styles for this template -->
+    <!--<link href="css/business-casual.css" rel="stylesheet">-->
+
+    <link href="<?php echo get_stylesheet_uri(); ?>" type="text/css" rel="stylesheet">
+    <?php // wp_enqueue_script('jquery');
+      // igual no es realmente necesaria
+    ?>
+  </head>
+
+  <body>
+  <?php wp_head(); ?>
